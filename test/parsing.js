84e8fed8773c15b95242092fd7ddbbbcf6884cc9
martin <martin@azumo.co>
2019-10-04 10:43:02 -0300

    parsing output

--- test/parsing.js
+++ test/parsing.js
@@ -4,7 +4,12 @@ const assert = require('assert');
 describe('moment', () => {
   describe('is valid', () => {
     it('parse date 2011-11-28 is valid', async () => {
-      const m = moment('02011-11-28');
+      const m = moment('2011-11-28');
+      assert.ok(m.isValid());
+    });
+
+    it('parse date 31 12 2018 with format is valid', async () => {
+      const m = moment('31 12 2018', 'DD MM YYYY');
       assert.ok(m.isValid());
     });
   });
